c5124dccdc7332d15374be8df9f6164bb1489576
Shashi Kant <shashikant_23en516@dtu.ac.in>
2025-08-04 13:07:00 +0530

    Revert "Update SiteFinancials.js"
    This reverts commit c08fa00f47223276903ac7776e8a72fa5b5cada0.

--- Backend/Routes/SiteFinancials.js
+++ Backend/Routes/SiteFinancials.js
@@ -166,7 +166,7 @@ router.get("/sites/:siteID/financial-summary", authenticateToken, async (req, re
     }
 
     try {
-        const siteObjectId = siteID;
+        const siteObjectId = new mongoose.Types.ObjectId(siteID);
         const parsedMonth = parseInt(month);
         const parsedYear = parseInt(year);
 
